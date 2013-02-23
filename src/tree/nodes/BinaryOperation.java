26ccd7a2bb45e9766863a13c5258b8cf103b3c8d
David Santiago Barrera <d4v1x.sb@gmail.com>
2013-02-23 14:56:59 +0000

    added labels multiply and subtract in Binary Operation.

--- src/tree/nodes/BinaryOperation.java
+++ src/tree/nodes/BinaryOperation.java
@@ -2,6 +2,8 @@ package tree.nodes;
 
 public class BinaryOperation {
     public static  final BinaryOperation add = new BinaryOperation("add", '+');
+    public static  final BinaryOperation subtract = new BinaryOperation("subtract", '-');
+    public static  final BinaryOperation mul = new BinaryOperation("mul", '*');
     
     private final String name;
     private final char operator;
