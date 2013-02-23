9166b71076efd8b022532dc04d026bba40cf358e
David Santiago Barrera <d4v1x.sb@gmail.com>
2013-02-23 14:55:04 +0000

    added operations to multiply and subtract numbers calculator.

--- src/tree/calculator/NumberCalculator.java
+++ src/tree/calculator/NumberCalculator.java
@@ -17,4 +17,36 @@ public class NumberCalculator extends Calculator {
     public Integer add(Integer arg0, Integer arg1) {
         return arg0 + arg1;
     }
+
+    public Double subtract(Double arg0, Double arg1) {
+        return arg0 - arg1;
+    }
+
+    public Double subtract(Double arg0, Integer arg1) {
+        return arg0 - arg1;
+    }
+
+    public Double subtract(Integer arg0, Double arg1) {
+        return arg0 - arg1;
+    }
+
+    public Integer subtract(Integer arg0, Integer arg1) {
+        return arg0 - arg1;
+    }
+
+    public Double mul(Double arg0, Double arg1) {
+        return arg0 * arg1;
+    }
+
+    public Double mul(Double arg0, Integer arg1) {
+        return arg0 * arg1;
+    }
+
+    public Double mul(Integer arg0, Double arg1) {
+        return arg0 * arg1;
+    }
+
+    public Integer mul(Integer arg0, Integer arg1) {
+        return arg0 * arg1;
+    }
 }
