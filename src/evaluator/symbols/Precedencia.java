db359c8433ec8f6e4bbf1ff2298a74ce23d98909
David Santiago Barrera <d4v1x.sb@gmail.com>
2013-04-05 18:12:13 +0100

    Add Precedence values.

--- src/evaluator/symbols/Precedencia.java
+++ src/evaluator/symbols/Precedencia.java
@@ -1,11 +1,22 @@
 package evaluator.symbols;
 
 class Precedencia {
-    public static final Precedencia funtion = new Precedencia();
-    public static final Precedencia empowerment = new Precedencia();
-    public static final Precedencia roots = new Precedencia();
-    public static final Precedencia multiplication = new Precedencia();
-    public static final Precedencia division = new Precedencia();
-    public static final Precedencia sum = new Precedencia();
-    public static final Precedencia subtraction = new Precedencia();
+    public static final Precedencia funtion = new Precedencia(4);
+    public static final Precedencia empowerment = new Precedencia(3);
+    public static final Precedencia roots = new Precedencia(3);
+    public static final Precedencia multiplication = new Precedencia(2);
+    public static final Precedencia division = new Precedencia(2);
+    public static final Precedencia sum = new Precedencia(1);
+    public static final Precedencia subtraction = new Precedencia(1);
+    private final Integer value;
+
+    public Precedencia(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+    
+    
 }
